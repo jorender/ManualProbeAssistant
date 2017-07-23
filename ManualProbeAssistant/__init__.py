b3c7ad29f35dd270ca63cdd73770d7251df70c48
Joshua Orender <stin93r@gmail.com>
2017-07-23 16:18:50 -0500

    Pull necessary config values with M503

--- ManualProbeAssistant/__init__.py
+++ ManualProbeAssistant/__init__.py
@@ -11,49 +11,51 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
+
 class ManualProbeAssistantPlugin(octoprint.plugin.SettingsPlugin,
                                  octoprint.plugin.AssetPlugin,
                                  octoprint.plugin.TemplatePlugin):
 
-	##~~ SettingsPlugin mixin
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
+    # SettingsPlugin mixin
 
-	##~~ AssetPlugin mixin
+    def get_settings_defaults(self):
+        return dict(
+            # put your plugin's default settings here
+        )
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/ManualProbeAssistant.js"],
-			css=["css/ManualProbeAssistant.css"],
-			less=["less/ManualProbeAssistant.less"]
-		)
+    # AssetPlugin mixin
 
-	##~~ Softwareupdate hook
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return dict(
+            js=["js/ManualProbeAssistant.js"],
+            css=["css/ManualProbeAssistant.css"],
+            less=["less/ManualProbeAssistant.less"]
+        )
 
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			ManualProbeAssistant=dict(
-				displayName="ManualProbeAssistant Plugin",
-				displayVersion=self._plugin_version,
+    # Softwareupdate hook
 
-				# version check: github repository
-				type="github_release",
-				user="jorender",
-				repo="ManualProbeAssistant",
-				current=self._plugin_version,
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+        # for details.
+        return dict(
+            ManualProbeAssistant=dict(
+                displayName="ManualProbeAssistant Plugin",
+                displayVersion=self._plugin_version,
 
-				# update method: pip
-				pip="https://github.com/jorender/ManualProbeAssistant/archive/{target_version}.zip"
-			)
-		)
+                # version check: github repository
+                type="github_release",
+                user="jorender",
+                repo="ManualProbeAssistant",
+                current=self._plugin_version,
+
+                # update method: pip
+                pip="https://github.com/jorender/ManualProbeAssistant/archive/{target_version}.zip"
+            )
+        )
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
@@ -61,12 +63,12 @@ class ManualProbeAssistantPlugin(octoprint.plugin.SettingsPlugin,
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "ManualProbeAssistant Plugin"
 
+
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = ManualProbeAssistantPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = ManualProbeAssistantPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
--- ManualProbeAssistant/static/js/ManualProbeAssistant.js
+++ ManualProbeAssistant/static/js/ManualProbeAssistant.js
@@ -8,11 +8,70 @@ $(function() {
     function ManualProbeAssistantViewModel(parameters) {
         var self = this;
 
+        self.delta_l = ko.observable();
+        self.delta_r = ko.observable();
+        self.delta_z = ko.observable();
+
+        self.trim_x = ko.observable();
+        self.trim_y = ko.observable();
+        self.trim_z = ko.observable();
+
+        self.fromCurrentData = function(data){
+            //var match665 = "/[lrz]:?\s+(\d+\.\d+)/i";
+            var expected_command_re = /M66[56]/;
+
+            for (var i = 0; i < data.logs.length; i++){
+                var message = data.logs[i];
+                if(expected_command_re.test(message)){
+                    var re = /M665.*L(-?\d+.\d{1,4})/g
+                    if((match = re.exec(message)) != null)
+                    {
+                        self.delta_l(match[1]);
+                    }
+
+                    re = /M665.*R(-?\d+.\d{1,4})/g
+                    if((match = re.exec(message)) != null)
+                    {
+                        self.delta_r(match[1]);
+                    }
+
+                    re = /M665.*Z(-?\d+.\d{1,4})/g
+                    if((match = re.exec(message)) != null)
+                    {
+                        self.delta_z(match[1]);
+                    }
+
+                    re = /M666.*X(-?\d+.\d{1,4})/g
+                    if((match = re.exec(message)) != null)
+                    {
+                        self.trim_x(match[1]);
+                    }
+
+                    re = /M666.*Y(-?\d+.\d{1,4})/g
+                    if((match = re.exec(message)) != null)
+                    {
+                        self.trim_y(match[1]);
+                    }
+
+                    re = /M666.*Z(-?\d+.\d{1,4})/g
+                    if((match = re.exec(message)) != null)
+                    {
+                        self.trim_z(match[1]);
+                    }
+                }
+            }
+        }
+
+        self.getSettings = function(){
+
+           var ret= OctoPrint.control.sendGcode('M503');
+        };
+
         // assign the injected parameters, e.g.:
         // self.loginStateViewModel = parameters[0];
         // self.settingsViewModel = parameters[1];
 
-        // TODO: Implement your plugin's view model here.
+
     }
 
     // view model class, parameters for constructor, container to bind to
@@ -23,6 +82,6 @@ $(function() {
         [ /* "loginStateViewModel", "settingsViewModel" */ ],
 
         // e.g. #settings_plugin_ManualProbeAssistant, #tab_plugin_ManualProbeAssistant, ...
-        [ /* ... */ ]
+        [ "#tab_plugin_ManualProbeAssistant" ]
     ]);
 });
